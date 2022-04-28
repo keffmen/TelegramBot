1a98fdfd4d668c58877e3dfaf08f2120c752768e
keffmen <bylka500@gmail.com>
2022-04-29 04:57:59 +0700

    стабильная версия Telebot 0,3

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import asyncio
 
-from aiogram import Bot, Dispatcher, executor
-from config import BOT_TOKEN
-
-loop = asyncio.get_event_loop()
-bot = Bot(BOT_TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot, loop=loop)
 import telebot
 from telebot import types
 
@@ -129,7 +122,3 @@ def user_reg(message):
 
 
 bot.polling()
-
-if __name__ == "__main__":
-    from handlers import dp, send_to_admin
-    executor.start_polling(dp, on_startup=send_to_admin)
